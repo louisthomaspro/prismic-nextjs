2e00d5c80a8bedd62c5c103540f4d09eba6423ab
Louis THOMAS <louisthomas.pro@gmail.com>
2023-08-25 10:39:24 +0800

    Add prismic js script

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import AnnouncementBar from "@/slices/AnnouncementBar";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <div>{children}</div>
       </body>
+      <Script src="https://static.cdn.prismic.io/prismic.js?new=true&repo=nextjs-pro" />
     </html>
   );
 }
